680194870a5639ae4544fa38b4c64d9f9b73d484
iqbal-dev <pkmdiqbal@gmail.com>
2024-08-12 23:23:44 +0600

    [add] academic query and update base query with set headers token

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,3 +1,7 @@
+import { useGetAllSemesterQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+
 export default function AcademicSemester() {
+  const { data } = useGetAllSemesterQuery(undefined);
+  console.log("🚀 ~ AcademicSemester ~ data:", data);
   return <div>AcademicSemester</div>;
 }
