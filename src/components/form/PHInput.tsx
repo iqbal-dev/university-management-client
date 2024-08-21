3f781bb7fce8d965787c502578f09a075a2ef35d
iqbal-dev <pkmdiqbal@gmail.com>
2024-08-21 22:17:13 +0600

    [update] ph input

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,4 +1,4 @@
-import { Input, Typography } from "antd";
+import { Form, Input } from "antd";
 import { Controller } from "react-hook-form";
 type TInputProps = {
   name: string;
@@ -6,13 +6,27 @@ type TInputProps = {
   label?: string;
   placeholder?: string;
 };
-export default function PHInput({ name, type, label,placeholder }: TInputProps) {
+export default function PHInput({
+  name,
+  type,
+  label,
+  placeholder,
+}: TInputProps) {
   return (
     <div style={{ marginBottom: "20px" }}>
-      {label ? <Typography.Title level={5}>{label}</Typography.Title> : null}
       <Controller
         name={name}
-        render={({ field }) => <Input placeholder={placeholder} id={name} {...field} type={type} />}
+        render={({ field }) => (
+          <Form.Item label={label}>
+            <Input
+              placeholder={placeholder}
+              id={name}
+              {...field}
+              type={type}
+              size="large"
+            />
+          </Form.Item>
+        )}
       />
     </div>
   );
